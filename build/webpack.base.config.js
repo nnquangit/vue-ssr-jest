ca2c319a8104998e3d4fd911583285e145ef9970
Quang Ngo <nnquangit@gmail.com>
2018-06-09 01:59:32 +0700

    Init

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -10,79 +10,79 @@ const isProd = process.env.NODE_ENV === 'production'
 const resolve = (file) => path.resolve(__dirname, file)
 
 const config = {
-  devtool: isProd ? false : '#cheap-module-eval-source-map',
-  mode: isProd ? 'production' : 'development',
-  performance: {
-    maxEntrypointSize: 1024 * 1024,
-    maxAssetSize: 500 * 1024,
-    hints: isProd ? 'warning' : false
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({uglifyOptions: {warnings: false, compress: true, output: {comments: false}}})
-    ],
-    noEmitOnErrors: true,
-  },
-  output: {
-    path: resolve('../public'),
-    publicPath: '/',
-    filename: '[name].[hash].js'
-  },
-  resolve: {
-    extensions: ['*', '.js', '.json', '.vue'],
-    alias: {
-      '@': resolve('src'),
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
-  module: {
-    noParse: /es6-promise\.js$/, // avoid webpack shimming process
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          hotReload: true
+    devtool: isProd ? false : '#cheap-module-eval-source-map',
+    mode: isProd ? 'production' : 'development',
+    performance: {
+        maxEntrypointSize: 1024 * 1024,
+        maxAssetSize: 500 * 1024,
+        hints: isProd ? 'warning' : false
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({uglifyOptions: {warnings: false, compress: true, output: {comments: false}}})
+        ],
+        noEmitOnErrors: true,
+    },
+    output: {
+        path: resolve('../public'),
+        publicPath: '/',
+        filename: '[name].[hash].js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.json', '.vue'],
+        alias: {
+            '@': resolve('src'),
+            'vue$': 'vue/dist/vue.common.js'
         }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: ['vue-style-loader', 'css-loader'],//MiniCssExtractPlugin.loader
-      },
-      {
-        test: /\.styl$/,
-        loader: ['vue-style-loader', 'css-loader', 'stylus-loader']
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {limit: 10000, name: 'fonts/[name].[hash:7].[ext]'}
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {limit: 10000, name: 'img/[name].[hash:7].[ext]'}
-      }
+    },
+    module: {
+        noParse: /es6-promise\.js$/, // avoid webpack shimming process
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    hotReload: true
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ['vue-style-loader', 'css-loader'],//MiniCssExtractPlugin.loader
+            },
+            {
+                test: /\.styl$/,
+                loader: ['vue-style-loader', 'css-loader', 'stylus-loader']
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {limit: 10000, name: 'fonts/[name].[hash:7].[ext]'}
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {limit: 10000, name: 'img/[name].[hash:7].[ext]'}
+            }
+        ]
+    },
+    plugins: [
+        new HardSourceWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        // new MiniCssExtractPlugin({
+        //   filename: "[name].[hash].css",
+        //   chunkFilename: "[id].[hash].css"
+        // }),
+        new VueLoaderPlugin(),
     ]
-  },
-  plugins: [
-    new HardSourceWebpackPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-    }),
-    // new MiniCssExtractPlugin({
-    //   filename: "[name].[hash].css",
-    //   chunkFilename: "[id].[hash].css"
-    // }),
-    new VueLoaderPlugin(),
-  ]
 }
 
 module.exports = config
--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -6,84 +6,84 @@ const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = merge(base, {
-  entry: {
-    app: './src/entry-client.js'
-  },
-  optimization: {
-    runtimeChunk: true,
-    splitChunks: {
-      chunks: "async",
-      minSize: 1000,
-      minChunks: 2,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 1,
-          priority: -20,
-          reuseExistingChunk: true,
+    entry: {
+        app: './src/entry-client.js'
+    },
+    optimization: {
+        runtimeChunk: true,
+        splitChunks: {
+            chunks: "async",
+            minSize: 1000,
+            minChunks: 2,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 1,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                }
+            }
         }
-      }
-    }
-  },
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: "async",
-  //     minSize: 30000,
-  //     minChunks: 1,
-  //     maxAsyncRequests: 5,
-  //     maxInitialRequests: 3,
-  //     automaticNameDelimiter: '~',
-  //     name: true,
-  //     cacheGroups: {
-  //       vendors: {
-  //         name: 'vendors',
-  //         test: /[\\/]node_modules[\\/]/,
-  //         chunks: 'all',
-  //         priority: -10
-  //       },
-  //       styles: {
-  //         name: 'styles',
-  //         test: /\.css$/,
-  //         chunks: 'all',
-  //         enforce: true
-  //       },
-  //       default: {
-  //         minChunks: 2,
-  //         priority: -20,
-  //         reuseExistingChunk: true
-  //       }
-  //     }
-  //   },
-  //   runtimeChunk: {name: 'runtime'},
-  //   // noEmitOnErrors: true,
-  //   namedModules: true,
-  //   namedChunks: true,
-  //   minimize: true,
-  //   minimizer: [
-  //     new UglifyJSPlugin({
-  //       uglifyOptions: {
-  //         warnings: false,
-  //         compress: true,
-  //         output: {
-  //           comments: false,
-  //         },
-  //       }
-  //     })
-  //   ],
-  // },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      'process.env.VUE_ENV': '"client"'
-    }),
-    new VueSSRClientPlugin()
-  ]
+    },
+    // optimization: {
+    //   splitChunks: {
+    //     chunks: "async",
+    //     minSize: 30000,
+    //     minChunks: 1,
+    //     maxAsyncRequests: 5,
+    //     maxInitialRequests: 3,
+    //     automaticNameDelimiter: '~',
+    //     name: true,
+    //     cacheGroups: {
+    //       vendors: {
+    //         name: 'vendors',
+    //         test: /[\\/]node_modules[\\/]/,
+    //         chunks: 'all',
+    //         priority: -10
+    //       },
+    //       styles: {
+    //         name: 'styles',
+    //         test: /\.css$/,
+    //         chunks: 'all',
+    //         enforce: true
+    //       },
+    //       default: {
+    //         minChunks: 2,
+    //         priority: -20,
+    //         reuseExistingChunk: true
+    //       }
+    //     }
+    //   },
+    //   runtimeChunk: {name: 'runtime'},
+    //   // noEmitOnErrors: true,
+    //   namedModules: true,
+    //   namedChunks: true,
+    //   minimize: true,
+    //   minimizer: [
+    //     new UglifyJSPlugin({
+    //       uglifyOptions: {
+    //         warnings: false,
+    //         compress: true,
+    //         output: {
+    //           comments: false,
+    //         },
+    //       }
+    //     })
+    //   ],
+    // },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+            'process.env.VUE_ENV': '"client"'
+        }),
+        new VueSSRClientPlugin()
+    ]
 })
 
 module.exports = config
