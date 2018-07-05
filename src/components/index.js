5db71399d57f96a65396fd8a0492f54f3fadd30a
Quang Ngo <nnquangit@gmail.com>
2018-07-05 21:23:55 +0700

    Fix html

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import AppNoSSR from './Layout/NoSSR'
 import AppLoader from './Layout/Loader.vue'
-import AppNavbar from './Layout/Navbar.vue'
 import UserInfo from './UserInfo.vue'
 
 Vue.use({
     install(Vue) {
-        Vue.component('app-navbar', AppNavbar)
         Vue.component('app-loader', AppLoader)
         Vue.component('app-nossr', AppNoSSR)
         Vue.component('app-user-info', UserInfo)
--- src/pages/Master.vue
+++ src/pages/Master.vue
@@ -1,6 +1,18 @@
 <template>
     <div id="app">
-        <app-navbar/>
+        <b-navbar toggleable="md" type="light" variant="light">
+            <b-container>
+                <b-navbar-brand to="/">Brand</b-navbar-brand>
+                <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+                <b-collapse is-nav id="nav_collapse">
+                    <b-navbar-nav>
+                        <b-nav-item to="/login">Login</b-nav-item>
+                        <b-nav-item to="/protected">Protected</b-nav-item>
+                        <b-nav-item to="/serverrender">ServerRender</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
         <router-view></router-view>
     </div>
 </template>
